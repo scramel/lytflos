76ab064797013cad019a96d58167804d71a584d7
Orión Sánchez <orionmelgarejo@gmail.com>
2025-11-08 01:22:02 -0400

    Changed main home component import method to fix deployment issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import EncyclopediaView from '../views/EncyclopediaView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: import('../views/EncyclopediaView.vue'),
+      component: EncyclopediaView,
     },
     {
       path: '/about',
